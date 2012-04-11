36f937cb31e150cc90fccd4872a1a28e3d2d6784
chaosprinz <chaosprinz@siggiduenkel.de.vu>
2012-04-11 16:24:38 +0200

    added some documentation to the code

--- auto_power.rb
+++ auto_power.rb
@@ -8,19 +8,34 @@ $0 = "autoshotdown"
 @min_battery_work = 10
 @min_battery_critic = 4
 
+#=acpi_down_test
+#uses acpi to test the battery-state
+#@@return [Array] An array of two elements:
+#   -the first says if the battery is discharging or not
+#   -the second tells the charging-state in percent
 def acpi_down_test
   acpi = `acpi`
   [acpi.match(/Discharging/),acpi.match(/[\d]+%/).to_s.gsub('%','').to_i]
 end
 
+#=on_low_discharge
+#callback for discharging low-battery-event
+#@@return [TrueClass] makes a syscall, which uses zenity to throw a 
+#   window with a warning-message.
 def on_low_discharge
   fork {system " zenity --info --text 'akku ist bei #{acpi_down_test[1]}% Leistung. Das System wird demnächst heruntergefahren."}
 end
 
+#=on_critic_discharge
+#callback for critic charging-state-event
+#@@return [TrueClass] makes a syscall, which shutdown the pc using sudo shutdown
 def on_critic_discharge
   fork {system("sudo shutdown -h now")}
 end
 
+#=acpi_test_loop
+#endless loop whih uses acpi_down_test to get the time to run, and uses the correct
+#callback-methods
 def acpi_test_loop
   loop do
     charge,offstate = acpi_down_test[1],acpi_down_test[0]
